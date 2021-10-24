51e2e8060c1c78e97c13af8b93d74d7996720ef6
Orion <852192419@qq.com>
2021-10-24 12:25:26 +0800

    master

---
+++ 6.lv_obj基础对象/视频/hello.c
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-24 12:25:15
+ * @LastEditTime: 2021-10-24 12:25:15
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \LVGL\6.lv_obj基础对象\视频\hello.c
+ */
