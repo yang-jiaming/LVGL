1907796c85d5439e8e7f756c8ccf7650bbe112aa
Orion <852192419@qq.com>
2021-10-24 12:23:17 +0800

    master

--- 8.lv_style样式/视频/hello.c
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: your name
- * @Date: 2021-10-24 12:17:22
- * @LastEditTime: 2021-10-24 12:17:22
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: \LVGL\8.lv_style样式\视频\hello.c
- */
